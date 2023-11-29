f746535ed08215b93a70dc99cc32528cb43afdb5
Joel Rool de Melo Junior <joelmelo1302@gmail.com>
2023-11-29 00:25:34 -0400

    UPDATE: matriz_de_adjacencia

--- matriz_de_adjacencia.py
+++ matriz_de_adjacencia.py
@@ -5,18 +5,18 @@ class Grafo:
         for i in range(self.vertices):
             self.grafo.append([0]*self.vertices) # Cria uma matriz (uma lista de listas)
     
-    def adicionar_aresta(self,vertice_inicial,vertice_final):
-        self.grafo[vertice_inicial][vertice_final] = 1
-        self.grafo[vertice_final][vertice_inicial] = 1
+    def adicionar_aresta(self,vertice_inicial,vertice_final,peso):
+        self.grafo[vertice_inicial][vertice_final] = peso
+        self.grafo[vertice_final][vertice_inicial] = peso
     
     def print_grafo(self):
         for i in self.grafo:
             print(i)
 
 # Teste para vê se está funcionando
-g = Grafo(3)
-g.adicionar_aresta(vertice_inicial=0,vertice_final=1)
-g.adicionar_aresta(vertice_inicial=0,vertice_final=2)
-g.adicionar_aresta(vertice_inicial=0,vertice_final=3)
-g.adicionar_aresta(vertice_inicial=0,vertice_final=4)
+g = Grafo(5)
+g.adicionar_aresta(vertice_inicial=0,vertice_final=1,peso=5)
+g.adicionar_aresta(vertice_inicial=0,vertice_final=2,peso=10)
+g.adicionar_aresta(vertice_inicial=0,vertice_final=3,peso=15)
+g.adicionar_aresta(vertice_inicial=0,vertice_final=4,peso=20)
 g.print_grafo()
